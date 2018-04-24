eee5b17f0e10ae6e3dc6e90441cbbce2edf7282c
aubreezy0 <aubreezy0@gmail.com>
2018-04-24 13:35:29 -0400

    adds curl scripts for projects table_v1
    duplicated scripts from other folders

---
+++ scripts/projects/create-example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "http://localhost:4741/examples" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "example": {
+      "text": "'"${TITLE}"'",
+      "text": "'"${DIFFICULTY}"'"
+    }
+  }'
+
+echo
