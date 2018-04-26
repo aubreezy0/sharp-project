286502c187f63c46ac3dffce2552481395dd74f6
aubreezy0 <aubreezy0@gmail.com>
2018-04-26 15:54:17 -0400

    adds current user to PATCH
    adds current user to patch to limit the ability of users to delete
    projects added by others.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,7 +44,7 @@ class ProjectsController < OpenReadController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_project
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
